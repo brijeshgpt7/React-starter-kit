0769d90f2143c3b592cf16f10a8e4d698ef47d89
Apple <vgvinay2@gmail.com>
2017-09-02 17:05:02 +0530

    1SEssion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-//console.log('HI');
+console.log('HI');
+import 'babel-polyfill'; //ES^ to ES7
+import React from 'react';
+import {render} from 'react-dom';
+
+import '../src/styles/style.css';//webpack can
+// 	import '../styles/style.css';//webpack can
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css" ;
+
+
+const Home = React.createClass({
+	render: function() {
+		return(
+          <div className='jumbotron'> Hello ES5 Syntex </div>
+		);
+	}
+ });
+
+ const HomePage = React.createClass({
+	render: function() {
+		return(
+		  <div className='jumbotron'> 
+           <Home/>
+          HomePage ES5 Syntex </div>
+        );
+	}
+ });
+
+render(<HomePage/>, document.getElementById('app'));
+
